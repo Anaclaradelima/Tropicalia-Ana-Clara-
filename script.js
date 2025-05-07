8698918c7c41f8e2c18608a17948ec8030341ca8
Anaclaradelima <161024456+Anaclaradelima@users.noreply.github.com>
2025-05-07 11:37:12 +0000

    terminamos aula 5

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
 
     let tamanhoAtualFonte = 1;
 
@@ -16,5 +18,12 @@ document.addEventListener('DOMContentLoaded', function () {
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 
     })
+    
+    botaoDeAcessibilidade.addEventListener('click', function (){
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+       })
 
 })
+
+
