d65cc10e533a26f20907fb183a5d455df44cc609
Anaclaradelima <161024456+Anaclaradelima@users.noreply.github.com>
2025-05-28 11:00:15 +0000

    terminamo a aula 6

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
     const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
     const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
+    const alternaContraste = document.getElementById('alterna-contraste')
 
     let tamanhoAtualFonte = 1;
 
@@ -23,6 +24,9 @@ document.addEventListener('DOMContentLoaded', function () {
         botaoDeAcessibilidade.classList.toggle('rotacao-botao');
         opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
        })
+      alternaContraste.addEventListener('click', function(){
+         document.body.classList.toggle('alto-contraste')
+ }) 
 
 })
 
